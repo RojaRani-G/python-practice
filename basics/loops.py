92abd04ecab37cce2b0a486ec68232b66eda71e1
RojaRani-G <rojarani0108@gmail.com>
2026-04-03 20:43:36 +0530

    added loops examples

--- basics/loops.py
+++ basics/loops.py
@@ -1,1 +1,13 @@
+# loops.py
 
+# For loop example
+print("For Loop Example:")
+for i in range(1, 6):
+    print(i)
+
+# While loop example
+print("\nWhile Loop Example:")
+count = 1
+while count <= 5:
+    print(count)
+    count += 1
